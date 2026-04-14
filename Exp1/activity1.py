052ced81fc66ac841341f481357d73d0ac5ad7da
shivanimahajan2911-eng <shivanimahajan2911@gmail.com>
2026-04-14 11:59:59 +0530

    Create activity1.py

---
+++ Exp1/activity1.py
@@ -0,0 +1,20 @@
+#Taking input of different data types 
+"""
+Created on Tue Apr 14 11:32:42 2026
+
+@author: shivani mahajan
+"""
+#taking inputs of different data types
+
+name = input("enter your name: ")
+age =  int(input("enter your age: "))
+height = float(input("enter your height: "))
+is_student = input("are you student? (true/flase): ")
+
+# printing the inputs 
+print("\nentered details:")
+print("name:",name)
+print("age:", age) 
+print("height:", height)
+print("is student:", is_student)   
+       
